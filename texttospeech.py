2e2f363cc0a29192dda0028a38fcefe8feb34a02
Christian Eilers <christian@eilers.cl>
2023-08-26 19:54:21 -0700

    Added text to speech with caching

--- texttospeech.py
+++ texttospeech.py
@@ -1,8 +1,9 @@
 from gtts import gTTS
 import os
-mytext = 'Welcome to geeksforgeeks!'
-language = 'en'
-tts = gTTS(text=mytext, lang=language, slow=False)
-tts.save("welcome.mp3")
-# Playing the converted file
-os.system("mpg123 welcome.mp3")
+
+
+def speak(self, intent, text, language="en"):
+    if not os.path.exists(f"./audio/{intent}.mp3"):
+        tts = gTTS(text=text, lang=language, slow=False)
+        tts.save(f"./audio/{intent}.mp3")
+    os.system(f"mpg123 ./audio/{intent}.mp3")
