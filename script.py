32209a5aa569eaa27fd65d83eb5c4655193a6ded
Christian Eilers <christian@eilers.cl>
2023-08-31 20:00:00 +0100

    Added MQTT connection

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ import pvrhino as pvrhino
 from dotenv import load_dotenv
 
 from texttospeech import speak
+import paho.mqtt.client as mqtt
 
 import os
 
@@ -178,6 +179,20 @@ def get_microphone_index_by_name(microphone_name, devices):
     return index
 
 
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("$SYS/#")
+
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.payload))
+
+
 def main():
     PorcupineClient.show_audio_devices()
 
@@ -202,6 +217,12 @@ def main():
         keywords=['jarvis'],
         rhino_client=rhino
     )
+
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect("mqtt.eclipseprojects.io", 1883, 60)
     porcupine_client.start_recorder()
 
 
